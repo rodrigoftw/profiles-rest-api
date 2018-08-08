de17e549149cbecd739b22f6e89852330abc5c8b
Rodrigo Andrade <rodrigoftw2@gmail.com>
2018-08-07 21:39:19 -0300

    Add POST, PUT, PATCH and DELETE functions to our APIView.

---
+++ src/profiles_project/profiles_api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+
+class HelloSerializer(serializers.Serializer):
+    """Serializes a name for testing ou APIView."""
+
+    name = serializers.CharField(max_length=10)
+
+    
